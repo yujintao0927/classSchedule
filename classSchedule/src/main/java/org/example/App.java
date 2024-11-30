f5e6d4ecab31156f57c2fb0dae5e9f387bda1a25
231250170 <231250170@smail.nju.edu.cn>
2024-11-30 20:27:51 +0800

    done

--- classSchedule/src/main/java/org/example/App.java
+++ classSchedule/src/main/java/org/example/App.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Hello world!
  */
 
+
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
