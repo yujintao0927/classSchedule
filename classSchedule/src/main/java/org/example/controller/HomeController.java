a512e304e05b9a72217d2578265577c187521434
231250099 <2879549937@qq.com>
2024-12-03 15:10:04 +0800

    1

--- classSchedule/src/main/java/org/example/controller/HomeController.java
+++ classSchedule/src/main/java/org/example/controller/HomeController.java
@@ -1,8 +1,9 @@
 package org.example.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 
     @GetMapping("/")
