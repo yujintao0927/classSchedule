02f1c07946418c5b7e053f154812cac84ab4a05d
231250099 <231250099@smail.nju.edu.cn>
2024-12-12 23:37:30 +0800

    只差classroom未修改

--- classSchedule/src/main/java/org/example/App.java
+++ classSchedule/src/main/java/org/example/App.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,10 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 
-
-@RestController
 @SpringBootApplication
-public class App {
+
+public class App extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return super.configure(builder) ;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
         System.out.println("Application started successfully!");
